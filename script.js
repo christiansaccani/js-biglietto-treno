4140800a98caf17f0d92856e3e3de597a089be82
christiansaccani <christian.saccani99@gmail.com>
2024-01-27 16:00:40 +0100

    JS Done with bonus

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ console.log("Prezzo attuale: €", price);
 } else {
 
   console.log('⛔️ value is NOT a number');
-  alert("Valore inserito non valido, la prego di ricaricare la pagina e riprovare!")
+  alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!")
 
 }
+
+const anni = +prompt ("Quanti anni ha il passeggero?");
+
+if (typeof anni === 'number' && !Number.isNaN(anni)) {
+
+    console.log('✅ value is a number');
+
+    if (anni < 18) {
+        console.log('Il passeggero è minorenne, si applica sconto!');
+        price = (price * 80 / 100).toFixed(2);     //arrotondare il risultato ai decimali
+        console.log("Prezzo finale: €", price);
+  
+        } else if (anni >= 65) {
+            console.log('Il passeggero è Over65, si applica sconto!');
+            price = (price * 60 / 100).toFixed(2);
+            console.log("Prezzo finale: €", price);
+      
+            } else {
+                console.log('Il passeggero non ha diritto ad alcuno sconto.');
+                console.log("Prezzo finale: €", price);
+            }
+    
+    } else {
+    
+      console.log('⛔️ value is NOT a number');
+      alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!")
+    
+    }
