81520edacce7fb96dd7958bc99e978f71000aae2
christiansaccani <christian.saccani99@gmail.com>
2024-01-27 16:19:19 +0100

    error added

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ var price;
 if (typeof kmtot === 'number' && !Number.isNaN(kmtot)) {
 
 console.log('✅ value is a number');
+document.getElementById('my_km').innerHTML = 
+` Il passeggero percorrerà ${kmtot} km. `;
+
 price = kmtot * 0.21;
 console.log("Prezzo attuale: €", price);
 
 } else {
 
-  console.log('⛔️ value is NOT a number');
-  alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!")
+    console.log('⛔️ value is NOT a number');
+    alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!");
+    throw new Error('⛔️ value is NOT a number');
 
 }
 
@@ -24,20 +28,37 @@ if (typeof anni === 'number' && !Number.isNaN(anni)) {
         console.log('Il passeggero è minorenne, si applica sconto!');
         price = (price * 80 / 100).toFixed(2);     //arrotondare il risultato ai decimali
         console.log("Prezzo finale: €", price);
+
+        document.getElementById('my_coupon').innerHTML = 
+        "Il passeggero ha diritto allo sconto Under18 del 20%!";
+
+        document.getElementById('price').innerHTML = 
+        ` Il prezzo finale è ${price} €. `;
+        
   
         } else if (anni >= 65) {
             console.log('Il passeggero è Over65, si applica sconto!');
             price = (price * 60 / 100).toFixed(2);
             console.log("Prezzo finale: €", price);
+
+            document.getElementById('my_coupon').innerHTML = 
+            "Il passeggero ha diritto allo sconto Over65 del 40%!";
+
+            document.getElementById('price').innerHTML = 
+            ` Il prezzo finale è ${price} €. `;
       
             } else {
                 console.log('Il passeggero non ha diritto ad alcuno sconto.');
                 console.log("Prezzo finale: €", price);
+
+                document.getElementById('price').innerHTML = 
+                ` Il prezzo finale è ${price} €. `;
             }
     
     } else {
     
-      console.log('⛔️ value is NOT a number');
-      alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!")
+        console.log('⛔️ value is NOT a number');
+        alert("Valore inserito non valido, la preghiamo di ricaricare la pagina e riprovare!");
+        throw new Error('⛔️ value is NOT a number');
     
     }
