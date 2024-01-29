b275b98e00b34bdb40d3bb6044c5014729157760
christiansaccani <christian.saccani99@gmail.com>
2024-01-29 22:42:28 +0100

    media done

--- snack/script.js
+++ snack/script.js
@@ -6,6 +6,7 @@ if (option === "celsius") {
 
     if (typeof celsius === 'number' && !Number.isNaN(celsius)) {
 
+        console.log('✅ Il valore inserito è: ', celsius);
         const fahr = celsius * 1.8 + 32;
         console.log('Il valore convertito in Fahrenheit è: ', fahr);
         document.getElementById('firstex').innerHTML = 
@@ -17,10 +18,31 @@ if (option === "celsius") {
         alert("Valore inserito non valido, riprovare!");
         location.reload(true);
     }
-    
-
 }
 
+else if (option === "media") {
 
+    const a = +prompt ("Fornisci 3 valori di cui calcolare la media, primo: ");
+    const b = +prompt ("Secondo: ");
+    const c = +prompt ("Terzo: ");
 
+    if (typeof a === 'number' && !Number.isNaN(a) && typeof b === 'number' && !Number.isNaN(b) && typeof c === 'number' && !Number.isNaN(c)) {
+
+        console.log('✅ I valori inseriti sono: ', a, b, c);
+
+        const sum = (a + b + c);
+        const media = (sum / 3).toFixed(2);
+        console.log('La media dei valori è: ', +media);
+
+        document.getElementById('secondex').innerHTML = 
+        ` La media dei valori ${a}, ${b}, ${c} è: ${media}`;
+
+    } else {
+
+        console.log('⛔️ value is NOT a number');
+        alert("Valore inserito non valido, riprovare!");
+        location.reload(true);
+    }
     
+}    
+
